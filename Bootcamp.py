9fb62352bdf9e206c517fb76af28f39ea945c18d
Prashanth <73587553+Prashanth-Santhanaraman@users.noreply.github.com>
2023-09-01 19:38:12 +0530

    Create Bootcamp.py

---
+++ Bootcamp.py
@@ -0,0 +1,18 @@
+
+# Taking user inputs
+a = float(input("Enter the first number (a): "))
+b = float(input("Enter the second number (b): "))
+
+# Floor Division
+floor_division_result = a // b
+
+# Power (a^b)
+power_result = a ** b
+
+# Check if variables are equal
+equal_check = a == b
+
+# Printing the results
+print("Floor Division:", floor_division_result)
+print("a power b:", power_result)
+print("Equal Check:", equal_check)
